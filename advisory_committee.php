dc8d6d6e84525457a24f4d85455e22cbeea3b399
Harsh <harshpatil7678@gmail.com>
2025-03-18 20:11:57 +0530

    fixed adv comittee table

--- advisory_committee.php
+++ advisory_committee.php
@@ -1,135 +1,64 @@
-<!DOCTYPE html>
-
-<html lang="en">
-
 <?php include 'header.php'; ?>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
- <style>
-
-    th,td{
-
-        padding: 10px;
-
-		text-align: left;
-
-    }
-
-    .nav-tabs > li > a:hover {
-
-        border-color: #57bc90 #57bc90 #57bc90;
-
-    }
-
-</style>
-
-
-
-<div class="banner">
-
-    <div class="">
-
-        <div class="" style="margin-left:0 !important;padding:0 !important;margin-right:0 !important;">
-
-            <div class="row" style="padding:0 !important;">
-
-                <div class="col-md-12" style="padding:0 !important;">
-
-
-
-                    <ol class="breadcrumb">
-
-                        <li><a href="./icnte.php">Home</a></li>
-
-                        <li><a href="javascript::">People</a></li>
-
-                        <li class="active">Advisory Committees</li>
-
-                    </ol>
-
-                </div>
-
-            </div>
-
-        </div>
-
-    </div>
-
+<!-- Breadcrumb Navigation -->
+<div class="bg-gray-100 py-3">
+  <div class="container mx-auto px-4">
+    <nav aria-label="breadcrumb" class="text-sm">
+      <ol class="list-reset flex text-gray-600">
+        <li>
+          <a href="icnte.php" class="text-blue-600 hover:underline">Home</a>
+        </li>
+        <li>
+          <span class="mx-2">/</span>
+        </li>
+        <li>
+          <a href="#" class="text-blue-600 hover:underline">People</a>
+        </li>
+        <li>
+          <span class="mx-2">/</span>
+        </li>
+        <li class="font-bold text-gray-800">Advisory Committee</li>
+      </ol>
+    </nav>
+  </div>
 </div>
 
-<div class="container">
-
-    <h2 style="text-align: center;">Advisory Committee</h2>
-
-    <hr class="style17">
-
+<!-- Page Heading -->
+<div class="container mx-auto px-4 my-8">
+  <h2 class="text-center text-3xl font-bold">Advisory Committee</h2>
+  <hr class="my-4 border-t-2 border-gray-300">
 </div>
 
-<div class="container-fluid"> 
-
- 
-
-	<div class="col-md-10 col-md-offset-1">
-
-			<div style="margin-bottom:15px;">
-
-	<table class="table table-striped table-responsive table-condensed" >   
-
-		<?php
-
-		include 'connection.php'; 
-
-		$sql="SELECT * FROM advisory_committees";
-
-		$result=mysqli_query($db,$sql);
-
-				while($row = mysqli_fetch_array($result))
-
-					{
-
-						$name=$row['name']; 
-
-						$designation=$row['designation'];
-
-						$institute=$row['institute'];?>
-
-											
-
-
-
+<!-- Advisory Committee Table -->
+<div class="container mx-auto px-4">
+  <div class="overflow-x-auto">
+    <table class="min-w-full bg-white border border-gray-200">
+      <thead class="bg-gray-200">
         <tr>
-
-      <td class="col-md-2"><?php echo "$name";?></td>
-
-      <td class="col-md-2"><?php echo "$designation";?></td>
-
-      <td class="col-md-4"><?php echo "$institute";?></td>
-
-    </tr>    
-
-		<?php
-
-		}
-
-		?>	
-
-     
-
-</table>
-
-
-
+          <th class="px-4 py-2 text-left">Name</th>
+          <th class="px-4 py-2 text-left">Designation</th>
+          <th class="px-4 py-2 text-left">Organization</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+          include 'connection.php'; 
+          $sql = "SELECT * FROM advisory_committees";
+          $result = mysqli_query($db, $sql);
+          while($row = mysqli_fetch_array($result)) {
+            $name = $row['name']; 
+            $designation = $row['designation'];
+            $institute = $row['institute'];
+        ?>
+        <tr class="border-t hover:bg-gray-50">
+          <td class="px-4 py-2"><?php echo htmlspecialchars($name); ?></td>
+          <td class="px-4 py-2"><?php echo htmlspecialchars($designation); ?></td>
+          <td class="px-4 py-2"><?php echo htmlspecialchars($institute); ?></td>
+        </tr>
+        <?php } ?>
+      </tbody>
+    </table>
+  </div>
 </div>
 
-</div>
-
-</div>	
-
-
-
-	   <?php include 'footer.php'; ?>
-
-</html>
+<?php include 'footer.php'; ?>
