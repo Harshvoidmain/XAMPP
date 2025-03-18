28d81ab5d23eb09a44440b42f2bd80af0f579f5a
Harsh <harshpatil7678@gmail.com>
2025-03-18 20:29:28 +0530

    Revert "table fixed again"
    This reverts commit 0d2d11bbffb93d155eb79fcaa144a3fb9faab23c.

--- reviewers_panel.php
+++ reviewers_panel.php
@@ -6,7 +6,7 @@
     <nav aria-label="breadcrumb" class="text-sm">
       <ol class="list-reset flex text-gray-600">
         <li>
-          <a href="icnte.php" class="text-blue-600 hover:underline">Home</a>
+          <a href="index.php" class="text-blue-600 hover:underline">Home</a>
         </li>
         <li>
           <span class="mx-2">/</span>
@@ -17,7 +17,7 @@
         <li>
           <span class="mx-2">/</span>
         </li>
-        <li class="font-bold text-gray-800">Advisory Committee</li>
+        <li class="font-bold text-gray-800">Reviewer's Panel</li>
       </ol>
     </nav>
   </div>
@@ -25,11 +25,11 @@
 
 <!-- Page Heading -->
 <div class="container mx-auto px-4 my-8">
-  <h2 class="text-center text-3xl font-bold">Advisory Committee</h2>
+  <h2 class="text-center text-3xl font-bold">Reviewer's Panel</h2>
   <hr class="my-4 border-t-2 border-gray-300">
 </div>
 
-<!-- Advisory Committee Table -->
+<!-- Reviewers Table -->
 <div class="container mx-auto px-4">
   <div class="overflow-x-auto">
     <table class="min-w-full bg-white border border-gray-200">
@@ -43,21 +43,23 @@
       <tbody>
         <?php
           include 'connection.php'; 
-          $sql = "SELECT * FROM advisory_committees";
+          $sql = "SELECT * FROM reviewer";
           $result = mysqli_query($db, $sql);
           while($row = mysqli_fetch_array($result)) {
-            $name = $row['name']; 
-            $designation = $row['designation'];
-            $institute = $row['institute'];
+            $salutation = $row['salutation'];
+            $name = $row['rewname']; 
+            $designation = $row['post'];
+            $institute = $row['organization'];
         ?>
         <tr class="border-t hover:bg-gray-50">
-          <td class="px-4 py-2"><?php echo htmlspecialchars($name); ?></td>
+          <td class="px-4 py-2"><?php echo htmlspecialchars($salutation . " " . $name); ?></td>
           <td class="px-4 py-2"><?php echo htmlspecialchars($designation); ?></td>
           <td class="px-4 py-2"><?php echo htmlspecialchars($institute); ?></td>
         </tr>
         <?php } ?>
       </tbody>
     </table>
+    <br></br>
   </div>
 </div>
 
